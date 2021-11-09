56448cc1dd7bf7f5fe16c61dc77733395ba95297
ag <anna.gorska@edu.uekat.pl>
2021-11-09 01:41:16 +0100

    Zadania lab_4

--- zad_1.py
+++ zad_1.py
@@ -1,7 +1,30 @@
-def zad_1(name: str, surname: str) -> str:
-    tekst = 'Cześć '+name+' '+surname+'!'
-    print(tekst)
+class Student:
+    def __init__(self, name: str, marks: int) -> None:
+        self._name = name
+        self._marks = marks
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, value: str) -> None:
+        self._name = value
+
+    @property
+    def marks(self) -> int:
+        return self._marks
+
+    @marks.setter
+    def marks(self, value: int):
+        self._marks = value
+
+    def is_passed(self) -> bool:
+        return self._marks > 50
 
 
 if __name__ == '__main__':
-    zad_1('Ania', 'G')
+    nn = Student('Student1', 70)
+    pp = Student('Student2', 40)
+    print(nn.is_passed())
+    print(pp.is_passed())
